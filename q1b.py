0e3afa998988a18af8b0754a58d1a30de4253207
Barnaby Collins <barnabyc@sky.com>
2021-03-30 22:28:30 +0100

    Okay I think it'll iterate properly now

--- q1b.py
+++ q1b.py
@@ -45,83 +45,91 @@ def ExpectationMaximisation(sequence, num_states):
     emissions = generateProbabilityMatrix(num_states, num_symbols)
     initialDistribution = generateProbabilityMatrix(1, num_states)[0]
 
-    # Convert structures to log space
-    [lTransitions, lEmissions, lInitialDistribution] = convertToLog([transitions, emissions, initialDistribution])
+    lastLikelihood = 0
+    likelihood = -1
 
-    # Initialise trellises
-    fTrellis = []
-    bTrellis = []
-    for o in range(sequenceLength):
-        fTrellis.append([])
-        bTrellis.append([])
-
-        for s in range(num_states):
-            fTrellis[i].append(0.0)
-            bTrellis[i].append(0.0)
-
-    # === FORWARD ALGORITHM ===
-    # Populate first row of trellis
-    for s in range(num_states):
-        fTrellis[0][s] = lInitialDistribution[s] + lEmissions[s][sequence[0]]
-    
-    # Populate the rest of the trellis
-    for l in range(1, sequenceLength):
-        for s in range(num_states):
-            fTrellis[l][s] = lEmissions[s][sequence[l]] + math.log(sum([math.exp(fTrellis[i][l-1] + lTransitions[i][s]) for i in range(num_states)]))
-
-    # === BACKWARD ALGORITHM ===
-    lastItem = sequenceLength - 1
-
-    # Populate last row of trellis
-    for s in range(num_states):
-        bTrellis[lastItem][s] = 1.0
-
-    # Populate the rest of the trellis
-    for l in range(lastItem, 0, -1):
-        for s in range(num_states):
-            bTrellis[l-1][s] = math.log(sum([math.exp(bTrellis[i][l] + lTransitions[s][i] + lEmissions[i][sequence[l]]) for i in range(num_states)]))
-    
-    # === UPDATE ESTIMATES ===
-    
-    # Probabilities of being in each state at each point in the sequence
-    gammas = []
-
-    # Probabilities of performing each transition at each point in the sequence
-    etas = []
-
-    for l in range(sequenceLength):
-        gammas.append([])
-        etas.append([])
-        for s in range(num_states):
-            gammas.append((fTrellis[l][s] + bTrellis[l][s]) - math.log(sum([math.exp(fTrellis[l][i] + bTrellis[l][i]) for i in range(num_states)])))
-
-            if (l != lastItem):
-                etas[l].append([])
-                for t in range(num_states):
-                    top = fTrellis[l][s] + lTransitions[s][t] + bTrellis[l+1][t] + lEmissions[t][sequence[l+1]]
-                    bottom = math.log(sum([sum([fTrellis[i][j] + lTransitions[i][j] + bTrellis[l+1][j] + lEmissions[j][sequence[l+1]] for j in range(num_states)]) for i in range(num_states)]))
-                    etas[l][s].append(top - bottom)
-    
-    newInitialDistribution = []
-    newTransitions = []
-    newEmissions = []
-
-    for s in range(num_states):
-        newInitialDistribution.append(math.exp(gammas[0][s]))
-        newTransitions.append([])
-        newEmissions.append([])
-
-        bottom = sum([math.exp(gammas[l][s]) for l in range(sequenceLength-1)])
-
-        for t in range(num_states):
-            top = sum([math.exp(etas[l][s][t]) for l in range(sequenceLength-1)])
-
-            newTransitions[s].append(top / bottom)
+    while (likelihood != lastLikelihood):
+        # Convert structures to log space
+        [lTransitions, lEmissions, lInitialDistribution] = convertToLog([transitions, emissions, initialDistribution])
         
-        for o in range(num_symbols):
-            top = sum([int(sequence[l] == o) * math.exp(gammas[l][s]) for l in range(sequenceLength)])
+        lastLikelihood = likelihood
+        
+        # Initialise trellises
+        fTrellis = []
+        bTrellis = []
+        for o in range(sequenceLength):
+            fTrellis.append([])
+            bTrellis.append([])
 
-            newEmissions.append(top / bottom)
+            for s in range(num_states):
+                fTrellis[i].append(0.0)
+                bTrellis[i].append(0.0)
+
+        # === FORWARD ALGORITHM ===
+        # Populate first row of trellis
+        for s in range(num_states):
+            fTrellis[0][s] = lInitialDistribution[s] + lEmissions[s][sequence[0]]
+        
+        # Populate the rest of the trellis
+        for l in range(1, sequenceLength):
+            for s in range(num_states):
+                fTrellis[l][s] = lEmissions[s][sequence[l]] + math.log(sum([math.exp(fTrellis[i][l-1] + lTransitions[i][s]) for i in range(num_states)]))
+
+        # === BACKWARD ALGORITHM ===
+        lastItem = sequenceLength - 1
+
+        # Populate last row of trellis
+        for s in range(num_states):
+            bTrellis[lastItem][s] = 1.0
+
+        # Populate the rest of the trellis
+        for l in range(lastItem, 0, -1):
+            for s in range(num_states):
+                bTrellis[l-1][s] = math.log(sum([math.exp(bTrellis[i][l] + lTransitions[s][i] + lEmissions[i][sequence[l]]) for i in range(num_states)]))
+        
+
+        # === UPDATE ESTIMATES ===
+        # Probabilities of being in each state at each point in the sequence
+        gammas = []
+
+        # Probabilities of performing each transition at each point in the sequence
+        etas = []
+
+        for l in range(sequenceLength):
+            gammas.append([])
+            etas.append([])
+            for s in range(num_states):
+                gammas.append((fTrellis[l][s] + bTrellis[l][s]) - math.log(sum([math.exp(fTrellis[l][i] + bTrellis[l][i]) for i in range(num_states)])))
+
+                if (l != lastItem):
+                    etas[l].append([])
+                    for t in range(num_states):
+                        top = fTrellis[l][s] + lTransitions[s][t] + bTrellis[l+1][t] + lEmissions[t][sequence[l+1]]
+                        bottom = math.log(sum([sum([fTrellis[i][j] + lTransitions[i][j] + bTrellis[l+1][j] + lEmissions[j][sequence[l+1]] for j in range(num_states)]) for i in range(num_states)]))
+                        etas[l][s].append(top - bottom)
+        
+        initialDistribution = []
+        transitions = []
+        emissions = []
+
+        for s in range(num_states):
+            initialDistribution.append(math.exp(gammas[0][s]))
+            transitions.append([])
+            emissions.append([])
+
+            bottom = sum([math.exp(gammas[l][s]) for l in range(sequenceLength-1)])
+
+            for t in range(num_states):
+                top = sum([math.exp(etas[l][s][t]) for l in range(sequenceLength-1)])
+
+                transitions[s].append(top / bottom)
+            
+            for o in range(num_symbols):
+                top = sum([int(sequence[l] == o) * math.exp(gammas[l][s]) for l in range(sequenceLength)])
+
+                emissions.append(top / bottom)
+
+        likelihood = sum([math.log(sum([math.exp(emissions[s][sequence[l]]) for s in range(num_states)])) for l in range(sequenceLength)])
     
 
 
