e0b73d900d37023f4982a287adb1df7eade64c90
Barnaby Collins <barnabyc@sky.com>
2021-04-04 22:07:54 +0100

    EVEN BIG CHUNGUS WORKS

--- test.py
+++ test.py
@@ -78,7 +78,69 @@ if (TEST_Q2D):
         ])
     ])
 
+    chungus = TreeNode([
+        TreeNode([
+            TreeNode([
+                TreeNode([], 'a'),
+                TreeNode([], 'b'),
+                TreeNode([], 'c'),
+                TreeNode([], 'd')
+            ]),
+            TreeNode([], 'e'),
+            TreeNode([], 'f'),
+            TreeNode([], 'g'),
+            TreeNode([
+                TreeNode([], 'h'),
+                TreeNode([], 'i')
+            ])
+        ]),
+        TreeNode([
+            TreeNode([
+                TreeNode([
+                    TreeNode([], 'j'),
+                    TreeNode([
+                        TreeNode([], 'k'),
+                        TreeNode([], 'm')
+                    ])
+                ]),
+                TreeNode([
+                    TreeNode([
+                        TreeNode([], 'n'),
+                        TreeNode([], 'o')
+                    ]),
+                    TreeNode([
+                        TreeNode([], 'p'),
+                        TreeNode([], 'q')
+                    ])
+                ]),
+                TreeNode([
+                    TreeNode([
+                        TreeNode([], 'r'),
+                        TreeNode([], 's')
+                    ]),
+                    TreeNode([
+                        TreeNode([], 't'),
+                        TreeNode([], 'u')
+                    ])
+                ]),
+                TreeNode([], 'v')
+            ]),
+            TreeNode([
+                TreeNode([
+                    TreeNode([], 'w'),
+                    TreeNode([], 'x')
+                ]),
+                TreeNode([], 'y')
+            ])
+        ]),
+        TreeNode([], 'z')
+    ])
+
     print([i.text for i in e.getConstraints()])
     print([i.text for i in f.getConstraints()])
+
     CHAOTIC_STEFAN = stefan.getConstraints()
     print([i.text for i in CHAOTIC_STEFAN], len(CHAOTIC_STEFAN))
+    
+    BIG_CHUNGUS = chungus.getConstraints()
+    print([i.text for i in BIG_CHUNGUS], len(BIG_CHUNGUS))
