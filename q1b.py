2cbe8d6980be5f46b2c84fde53bf54023cf21851
Barnaby Collins <barnabyc@sky.com>
2021-04-08 14:04:19 +0100

    yuck

--- q1b.py
+++ q1b.py
@@ -1,8 +1,29 @@
 import math
 from collections import OrderedDict
-from typing import Union
+import numpy as np
 
-def ExpectationMaximisation(sequence: str, num_states: int) -> tuple:
+def generateProbabilityMatrix(height: int, width: int) -> list:
+    """Generates and returns a randomised matrix where each row contains a discrete probability distribution
+    Parameters
+    - height = number of distributions
+    - width = number of items in each distribution
+    """
+
+    import random
+
+    matrix = []
+
+    for i in range(height):
+        # randpd2 from https://thehousecarpenter.wordpress.com/2017/02/22/generating-random-probability-distributions/
+
+        variates = [random.random() for i in range(width)]
+        s = sum(variates)
+        matrix.append([i/s for i in variates])
+    
+    return matrix
+
+
+def ExpectationMaximisation(sequence, num_states, alphabet = []):
     """Finds a local maximum set of parameters for an HMM with 'num_states' states and observed output 'sequence'
 
     Parameters:
@@ -20,3 +41,32 @@ def ExpectationMaximisation(sequence: str, num_states: int) -> tuple:
     )
     """
 
+    if (alphabet == []):
+        alphabet = list(OrderedDict.fromkeys(sequence).keys())
+
+    num_symbols = len(symbols)
+    len_sequence = len(sequence)
+
+    observed = [alphabet.index(s) for s in sequence]
+
+    [initialStates, endStates] =    generateProbabilityMatrix(2, num_states)
+    transitions =                   generateProbabilityMatrix(num_states, num_states)
+    emissions =                     generateProbabilityMatrix(num_states, num_symbols)
+
+    # === FORWARD ALGORITHM ===
+    fTrellis = [[] for o in len_sequence]
+    fTrellis[0] = [initialStates[s] * emissions[s][observed[0]] for s in range(num_states)]
+    for i, x in enumerate(observed)[1:]:
+        for s in range(num_states):
+            fTrellis[i].append(0)
+            for r in range(num_states):
+                fTrellis[i][s] += fTrellis[i-1][r] * transitions[r][s]
+            
+            fTrellis[i][s] *= emissions[s][x]
+    
+    lastItem = len_sequence - 1
+
+    bTrellis = [[] for o in len_sequence]
+    bTrellis[lastItem] = endStates
+    for i, x in reversed(enumerate(observed)):
+        pass # bruh
